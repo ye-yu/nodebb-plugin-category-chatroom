a83a88f7347c70ee86a9fe351c5f6a6e05149b3d
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2021-05-02 21:49:47 +0800

    Add widget renderer

--- library.js
+++ library.js
@@ -4,7 +4,10 @@
 	var User = require.main.require('./src/user');
 	var db = require.main.require('./src/database');
 	var winston = require.main.require('winston');
+	var express = require.main.require('express')
 	var sse = require("@toverux/expresse")
+	var fs = require("fs")
+	var path = require("path")
 
 	var constants = Object.freeze({
 		'name': "Category Chatroom",
@@ -13,6 +16,10 @@
 			'route': '/plugins/category-chatroom'
 		}
 	});
+
+	var widget = fs.readFileSync(path.join(__dirname, "/static/lib/client/widget.html"), {
+		encoding: "utf-8"
+	})
 
 	var LB = {};
 
@@ -27,13 +34,31 @@
 	};
 
 	LB.initRoute = function({ router }, callback) {
+		winston.verbose("[plugin/category-chatroom] Invoked router init")
+		router.use("/category-chatroom", express.static('assets'))
 		callback();
 	};
 
-	LB.initScript = function(data, callback) {
-		winston.verbose("[plugin/Category Chatroom] Invoked initScript, data:", data)
-		callback();
+	LB.initScript = function({ res }, callback) {
+		winston.verbose("[plugin/category-chatroom] Invoked initScript, data:", data)
+		callback(null, data);
 	};
+
+	LB.defineWidget = function(data, callback) {
+		winston.verbose("[plugin/category-chatroom] Requested plugin widget, widget body: " + widget)
+		data.push({
+			widget: "category_chatbot",
+			name: "Category Chatbot",
+			description: "Add chatbot to your specific category",
+			content: widget
+		})
+		callback(null, data)
+	}
+
+	LB.renderWidget = function(widgetObj, callback) {
+		widgetObj.html = widget
+		callback(null, widgetObj)
+	}
 
 	module.exports = LB;
 }(module));
