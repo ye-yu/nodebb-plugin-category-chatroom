9ce09439fe268673f1ee2127c5d975d1c1d103fa
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2021-05-05 07:20:17 +0800

    Add Joi validator; Add messaging library

--- library.js
+++ library.js
@@ -5,9 +5,68 @@
 	var db = require.main.require('./src/database');
 	var winston = require.main.require('winston');
 	var express = require.main.require('express')
-	var sse = require("@toverux/expresse")
 	var fs = require("fs")
 	var path = require("path")
+	var Joi = require("joi")
+	var Messaging = require("./lib/messaging").init({
+		db,
+		logging: winston.verbose
+	})
+
+	var schema = {
+		init: Joi.object({
+			chat_id: Joi.string().min(1).required(),
+			limit: Joi.number().min(1)
+		}),
+		message: Joi.object({
+			chat_id: Joi.string().min(1).required(),
+			message: Joi.string().min(1).required(),
+		})
+	}
+
+	var SocketPlugins = require.main.require('./src/socket.io/plugins');
+	SocketPlugins.categoryChatroom = {};
+	SocketPlugins.categoryChatroom.ping = function(socket, data, callback) { 
+		winston.verbose("[plugin/category-chatroom] Got ping:", data)
+		socket.emit("event:category-chatroom.pong", data)
+		callback(null, data)
+	};
+
+	var validateSocket = async function(socket) {
+		const { uid } = socket
+		winston.verbose("[plugin/category-chatroom] Got init from uid:" + uid)
+		if (!await User.exists(uid)) throw new Error(`${uid} is not a user!`)
+	}
+
+	SocketPlugins.categoryChatroom.init = async function(socket, data) {
+		await validateSocket(socket)
+		schema.init.validate(data)
+		const { chat_id, limit = 50 } = data
+
+		socket.emit("event:category-chatroom.pong", {
+			message: `Retrieving ${limit} rows of chat history of ${chat_id}`
+		})
+
+		const history = await Messaging.getMessages(chat_id, limit)
+		
+		socket.emit("event:category-chatroom.history", {
+			chat_id,
+			history
+		})
+
+		return data
+	};
+
+	SocketPlugins.categoryChatroom.message = async function(socket, data) {
+		await validateSocket(socket)
+		schema.message.validate(data)
+		const { chat_id, message } = data
+		const user = await User.getUsersFields(socket.uid, ['uid', 'username', 'picture']);
+		const now = Date.now()
+		winston.verbose("Got user info of:", {
+			user, now
+		})
+	}
 
 	var constants = Object.freeze({
 		'name': "Category Chatroom",
